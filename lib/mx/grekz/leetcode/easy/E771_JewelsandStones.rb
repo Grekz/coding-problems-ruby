fc1c6166fff6a492e03dedcf923d798c97be5239
grekz <vidbla.grekz@gmail.com>
2018-03-14 00:24:15 +0100

    RLCE-771. Jewels and Stones

---
+++ lib/mx/grekz/leetcode/easy/E771_JewelsandStones.rb
@@ -0,0 +1,9 @@
+# @author grekz
+class E771_JewelsandStones
+    # @param {String} j
+    # @param {String} s
+    # @return {Integer}
+    def num_jewels_in_stones(j, s)
+        s.count(j)
+    end
+end
