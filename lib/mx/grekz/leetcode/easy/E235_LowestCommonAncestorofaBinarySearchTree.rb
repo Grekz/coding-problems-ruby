a122577d11aff2f6135b0c1598498f70918f39fe
grekz <vidbla.grekz@gmail.com>
2018-03-17 14:20:22 +0100

    RLCE-235. Lowest Common Ancestor of a Binary Search Tree

---
+++ lib/mx/grekz/leetcode/easy/E235_LowestCommonAncestorofaBinarySearchTree.rb
@@ -0,0 +1,21 @@
+# @author grekz
+class E235_LowestCommonAncestorofaBinarySearchTree
+    # Definition for a binary tree node.
+    # class TreeNode
+    #     attr_accessor :val, :left, :right
+    #     def initialize(val)
+    #         @val = val
+    #         @left, @right = nil, nil
+    #     end
+    # end
+    
+    # @param {TreeNode} root
+    # @param {TreeNode} p
+    # @param {TreeNode} q
+    # @return {TreeNode}
+    def lowest_common_ancestor(root, p, q)
+        return lowest_common_ancestor(root.right, p, q) if ( root.val < p.val and root.val < q.val ) 
+        return lowest_common_ancestor(root.left,p, q) if ( root.val > p.val and root.val > q.val ) 
+        root
+    end
+end
