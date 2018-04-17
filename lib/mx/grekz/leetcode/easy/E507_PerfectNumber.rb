d838a50aca8fad6d299c452de8f320b3124ac563
grekz <vidbla.grekz@gmail.com>
2018-04-17 08:33:29 +0200

    RLCE-507. Perfect Number

---
+++ lib/mx/grekz/leetcode/easy/E507_PerfectNumber.rb
@@ -0,0 +1,8 @@
+# @author grekz
+class E507_PerfectNumber
+    # @param {Integer} num
+    # @return {Boolean}
+    def check_perfect_number(num)
+        [6,28,496,8128,33550336].include? num
+    end
+end
