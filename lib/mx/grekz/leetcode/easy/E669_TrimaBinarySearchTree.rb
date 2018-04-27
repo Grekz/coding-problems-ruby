a9851b00b763c51a819ce028986460cf9fe95e41
grekz <vidbla.grekz@gmail.com>
2018-04-27 23:52:47 +0200

    RLCE-669. Trim a Binary Search Tree

---
+++ lib/mx/grekz/leetcode/easy/E669_TrimaBinarySearchTree.rb
@@ -0,0 +1,24 @@
+# @author grekz
+class E669_TrimaBinarySearchTree
+    # Definition for a binary tree node.
+    # class TreeNode
+    #     attr_accessor :val, :left, :right
+    #     def initialize(val)
+    #         @val = val
+    #         @left, @right = nil, nil
+    #     end
+    # end
+    
+    # @param {TreeNode} root
+    # @param {Integer} l
+    # @param {Integer} r
+    # @return {TreeNode}
+    def trim_bst(root, l, r)
+        return root if (root == nil)
+        return trim_bst(root.right, l, r) if (root.val < l)
+        return trim_bst(root.left, l, r) if (root.val > r)
+        root.left = trim_bst(root.left, l, r)
+        root.right = trim_bst(root.right, l, r)
+        return root
+    end
+end
