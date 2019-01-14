49c0ccfd3c1be42799919777d0d5ecd6c4c31de4
Grekz <vidbla.grekz@gmail.com>
2019-01-14 08:07:57 +0100

    RLCE-701. Insert into a Binary Search Tree

---
+++ lib/mx/grekz/leetcode/medium/E701_InsertintoaBinarySearchTree.rb
@@ -0,0 +1,24 @@
+# @author grekz
+class E701_InsertintoaBinarySearchTree
+    # Definition for a binary tree node.
+    # class TreeNode
+    #     attr_accessor :val, :left, :right
+    #     def initialize(val)
+    #         @val = val
+    #         @left, @right = nil, nil
+    #     end
+    # end
+
+    # @param {TreeNode} root
+    # @param {Integer} val
+    # @return {TreeNode}
+    def insert_into_bst(root, val)
+        return TreeNode.new(val) if not root
+        if val < root.val
+            root.left = insert_into_bst(root.left, val) 
+        else
+            root.right = insert_into_bst(root.right, val) 
+        end
+        root
+    end
+end
