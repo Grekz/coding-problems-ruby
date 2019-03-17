dde1a759d491a12d3f0ab71d89d64a14fcc471b0
Grekz <vidbla.grekz@gmail.com>
2019-03-17 23:21:50 +0100

    RLCE-1013. Pairs of Songs With Total Durations Divisible by 60

---
+++ lib/mx/grekz/leetcode/easy/E1013_PairsofSongsWithTotalDurationsDivisibleby60.rb
@@ -0,0 +1,15 @@
+# @author grekz
+class E1013_PairsofSongsWithTotalDurationsDivisibleby60
+    # @param {Integer[]} time
+    # @return {Integer}
+    def num_pairs_divisible_by60(time)
+        res, cnt = 0, Array.new(60, 0)
+        time.each {
+            |x|
+            d = (60 - x % 60) % 60
+            res += cnt[d]
+            cnt[x % 60] += 1
+        } 
+        return res
+    end
+end
